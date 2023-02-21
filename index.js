e32c43154b42ce2eb95d74feca107b9943c263fb
Ronin Protheroe <ronin@kiwiscripter.xyz>
2023-02-21 09:41:35 -0600

    Removed unneeded `console.log`

--- index.js
+++ index.js
@@ -80,7 +80,6 @@ module.exports = class Gliph {
 
         let search = this.search(filter)
         if(!search.success) return { success: false, string: search.string}
-        console.log(search.array.length)
         let removed = []
         for(let i = 0; i < search.array.length; i++){
             fs.unlinkSync(path.join(this.tablePath, search.array[i].id))
